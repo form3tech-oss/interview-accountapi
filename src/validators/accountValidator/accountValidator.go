83f937fc563a30d8c808637ccce568be80312425
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-10-04 22:39:38 -0300

    Validator creation

---
+++ src/validators/accountValidator/accountValidator.go
@@ -0,0 +1,9 @@
+package accountValidator
+
+/*
+	Function to validate fields in AccountData
+	For example if value is negative or empty
+*/
+func Validate(accountData map[string]interface{}) bool {
+	return true
+}
