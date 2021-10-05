5cb202ebfca55e488d4e7d881a66128576df0265
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-10-04 22:36:04 -0300

    Create endpoints and its URLs

--- src/endpoint/endpoints.go
+++ src/endpoint/endpoints.go
@@ -1,6 +1,8 @@
 package endpoint
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,7 +12,19 @@ import (
 
 func GetHealth() {
 
-	response, err := http.Get(BASE_URL + HEALTH)
+	response, err := http.Get(HEALTH_URL)
+
+	if err != nil {
+		fmt.Printf(messages.REQ_NOT_FOUND+" with error %s", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+		defer response.Body.Close()
+	}
+}
+
+func GetAccounts() {
+	response, err := http.Get(ACCOUNT_URL)
 
 	if err != nil {
 		fmt.Printf(messages.REQ_NOT_FOUND+" with error %s", err)
@@ -18,4 +32,37 @@ func GetHealth() {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Println(string(data))
 	}
+}
+
+func PostAccount(accountData map[string]interface{}) {
+
+	fmt.Println(accountData)
+	jsonValue, err := json.Marshal(accountData)
+
+	response, err := http.Post(ACCOUNT_URL, APP_JSON, bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		fmt.Printf(messages.REQ_NOT_FOUND+" with error %s", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
+}
+
+func DeleteAccount(id string) {
+	client := &http.Client{}
+
+	request, err := http.NewRequest("DELETE", DELETE_URL+id, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer response.Body.Close()
 }
--- src/endpoint/urls.go
+++ src/endpoint/urls.go
@@ -1,4 +1,12 @@
 package endpoint
 
-const HEALTH string = "/v1/health"
-const BASE_URL string = ""
+import "strconv"
+
+var PORT int = 8080
+
+var BASE_URL = "http://localhost:" + strconv.Itoa(PORT)
+
+var HEALTH_URL string = BASE_URL + "/v1/health"
+var ACCOUNT_URL = BASE_URL + "/v1/organisation/accounts"
+var APP_JSON = BASE_URL + "application/json"
+var DELETE_URL = ACCOUNT_URL + "/"
