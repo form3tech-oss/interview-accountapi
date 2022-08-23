000f71e6c3a1cf43c11a4d4c57d406030fa848aa
Daniel Szekely <danszekely@gmail.com>
2022-08-23 14:47:20 -0400

    create Client constructor function
    First iteration of creating the client isntance. Unfortunately
    is is a more OOP approach than what is idiomataic to Go.
    
    Inspiration came form AWS SDK, Typescript and DDD (layerered
    architecture) experience.

--- src/main.go
+++ src/main.go
@@ -4,3 +4,26 @@ type AccountFetcher interface {
 	FetchAccount(params FetchAccountParams) FetchAccountResult
 }
 
+type AccountCreator interface {
+	CreateAccount()
+}
+
+type AccountRemover interface {
+	DeleteAccount()
+}
+
+type Client struct {
+	base_url string
+}
+
+type NewClientParams struct {
+	BaseUrl string
+}
+
+func NewClient(params *NewClientParams) *Client {
+	client := Client{
+		base_url: params.BaseUrl,
+	}
+
+	return &client
+}
