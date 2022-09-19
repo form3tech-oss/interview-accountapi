5aa17af9ca657717b4b2c9c5568c0e25cf1ce990
Gianni Massi <me@giannimassi.it>
2022-09-19 10:23:48 +0200

    tests: more integration tests

--- accountapi/integration_tests/accountapi_test.go
+++ accountapi/integration_tests/accountapi_test.go
@@ -27,12 +27,10 @@ func Test_integration(t *testing.T) {
 	t.Run("When creating a valid account, the account is created and the account can be fetched and deleted by ID", func(t *testing.T) {
 		// Setup library
 		c := accountapi.NewAccountAPIClient(accountapi.WithHTTPClient(http.DefaultClient), accountapi.WithHost(host))
-
 		ctx, cancel := context.WithTimeout(context.Background(), testRequestTimeout)
 		defer cancel()
 
 		id := uuid.New().String()
-
 		a := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
 
 		// Create account
@@ -52,16 +50,74 @@ func Test_integration(t *testing.T) {
 		fetchedAgain, err := c.Fetch(ctx, *created)
 		require.Equal(t, errors.New("unexpected status code: 404"), err)
 		require.Nil(t, fetchedAgain, "Account should have been deleted, but it was not")
+	})
 
+	t.Run("When creating account with existing id, an error is returned", func(t *testing.T) {
+		// Setup library
+		c := accountapi.NewAccountAPIClient(accountapi.WithHTTPClient(http.DefaultClient), accountapi.WithHost(host))
+		ctx, cancel := context.WithTimeout(context.Background(), testRequestTimeout)
+		defer cancel()
+
+		id := uuid.New().String()
+		a := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
+
+		// Create account
+		_, err := c.Create(ctx, a)
+		require.NoError(t, err)
+
+		b := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
+
+		// Fail creating account with same id
+		_, err = c.Create(ctx, b)
+		require.EqualError(t, err, "unexpected status code: 409")
 	})
 
 	t.Run("When fetching account by unexisting ID, an account is not returned", func(t *testing.T) {
+		c := accountapi.NewAccountAPIClient(accountapi.WithHTTPClient(http.DefaultClient), accountapi.WithHost(host))
+		ctx, cancel := context.WithTimeout(context.Background(), testRequestTimeout)
+		defer cancel()
+
+		id := uuid.New().String()
+		a := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
+
+		// Fetch account by unexistent ID
+		_, err := c.Fetch(ctx, a)
+		require.EqualError(t, err, "unexpected status code: 404")
 	})
 
 	t.Run("When deleting account by unexisting ID, an account is not deleted", func(t *testing.T) {
+		c := accountapi.NewAccountAPIClient(accountapi.WithHTTPClient(http.DefaultClient), accountapi.WithHost(host))
+		ctx, cancel := context.WithTimeout(context.Background(), testRequestTimeout)
+		defer cancel()
+
+		id := uuid.New().String()
+		a := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
+		a.Version = new(int64)
+
+		// Delete account by unexistent ID
+		err := c.Delete(ctx, a)
+		require.EqualError(t, err, "specified resource does not exist")
 	})
 
 	t.Run("When deleting account by incorrect version, an account is not deleted", func(t *testing.T) {
+		// Setup library
+		c := accountapi.NewAccountAPIClient(accountapi.WithHTTPClient(http.DefaultClient), accountapi.WithHost(host))
+		ctx, cancel := context.WithTimeout(context.Background(), testRequestTimeout)
+		defer cancel()
+
+		id := uuid.New().String()
+		a := accountData(id, "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c")
+
+		// Create account
+		created, err := c.Create(ctx, a)
+		require.NoError(t, err)
+
+		// Overwrite version with wrong value
+		*created.Version++
+
+		// Fetch account
+		err = c.Delete(ctx, *created)
+		require.EqualError(t, err, "specified version incorrect")
 	})
 }
 
